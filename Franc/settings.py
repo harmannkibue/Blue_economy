165ba7510cc2881feb2d02da6fabacf6102a121d
harmannkibue <harmannkibue@gmail.com>
2019-02-28 11:56:24 +0300

    Francis application commit

--- Franc/settings.py
+++ Franc/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR=os.path.join(BASE_DIR,'static')
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 STATIC_DIR,
 ]
+
+django_heroku.settings(locals())
